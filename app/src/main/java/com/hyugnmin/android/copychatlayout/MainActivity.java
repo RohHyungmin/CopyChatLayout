6110508d5f1b6f2f78306788b44593a6938e23cc
DESKTOP-7VD6D5S\besto <bestofall77@gmail.com>
2017-02-17 18:09:41 +0900

    init

---
+++ app/src/main/java/com/hyugnmin/android/copychatlayout/MainActivity.java
@@ -0,0 +1,23 @@
+package com.hyugnmin.android.copychatlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**채팅 UI
+ * http://ankyu.entersoft.kr/Lecture/android/listview_03.asp
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
